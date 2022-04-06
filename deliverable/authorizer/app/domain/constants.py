debcceed614dfe9826d0b0f41d80500efd4edb83
Andres Garcia <aagarciat@yahoo.com>
2022-04-06 17:06:27 -0500

    Delete unused foler

--- deliverable/authorizer/app/domain/constants.py
+++
@@ -1,6 +0,0 @@
-ACCOUNT_ALREADY_INITIALIZED = "account-already-initialized"
-ACCOUNT_NOT_INITIALIZED = "account-not-initialized"
-INSUFFICIENT_LIMIT = "insufficient-limit"
-CARD_NOT_ACTIVE = "card-not-active"
-HIGH_FREQUENCY_SMALL_INTERVAL = "high-frequency-small-interval"
-DOUBLED_TRANSACTION = "doubled-transaction"
